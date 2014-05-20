51e0a6f2a043981f44d3d2eaf39344bcb507f2b1
AlanFonderflick <chromatic.world@gmail.com>
2014-05-20 20:53:03 +0200

    Modele avec cle etrangere

--- Simulation/Models/CarnetEntretien.cs
+++ Simulation/Models/CarnetEntretien.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Simulation.Models
 {
-    public class CarnetEntretien
+    public class Service
     {
-        public int Id { get; set; }
-        public string Marque { get; set; }
-        public string Libelle { get; set; }
-        public int Kilometrage { get; set; }
-        public int DureeIntervention { get; set; }
+        [Key]
+        public int service_id { get; set; }
+        public string label { get; set; }
+        public int distance { get; set; }
+        public int duration { get; set; }
+        [ForeignKey("car_id")]
+        public int car_id { get; set; }
+    }
+
+    public class Garagist
+    {
+        [Key]
+        public int garagist_id { get; set; }
+        public string franchise { get; set; }
+        [ForeignKey("car_id")]
+        public int car_id { get; set; }
+        [ForeignKey("service_id")]
+        public int service_id { get; set; }
+    }
+
+    public class Car
+    {
+        [Key]
+        public int car_id { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        [ForeignKey("service_id")]
+        public int service_id { get; set; }
     }
 }
